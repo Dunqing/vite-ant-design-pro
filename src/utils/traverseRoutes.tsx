3a78bafbac888febd52f9056fe41c469ddd88b98
dengqing <1247748612@qq.com>
2022-04-07 14:41:11 +0800

    feat: support redirect

--- src/utils/traverseRoutes.tsx
+++ src/utils/traverseRoutes.tsx
@@ -1,10 +1,10 @@
 import { lazy } from 'react'
+import { Navigate } from 'react-router-dom'
 import type { RoutesType } from '../types/routes'
 
 export const traverseRoutes = (routes?: RoutesType): RoutesType => {
   return routes?.map((route) => {
-    let { component } = route
-
+    let { element, component, redirect } = route
 
     if (typeof component === 'string') {
       // remove ./ or ../
@@ -12,9 +12,14 @@ export const traverseRoutes = (routes?: RoutesType): RoutesType => {
       component = lazy(() => import(`$ROOT/src/pages/${name}.tsx`))
     }
 
+    if (redirect !== undefined) {
+      element = <Navigate replace to={redirect}></Navigate>
+    }
+
     return {
       ...route,
       component,
+      element,
       children: traverseRoutes(route.children),
     }
   }) as RoutesType
