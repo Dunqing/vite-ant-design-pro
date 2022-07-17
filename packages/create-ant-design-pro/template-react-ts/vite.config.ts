d94081d0feddb050ea1e09934292bc58a8d3be7a
qing.deng <dengqing0821@gmail.com>
2022-07-17 14:07:07 +0800

    chore: dose not use proxy

--- packages/create-ant-design-pro/template-react-ts/vite.config.ts
+++ packages/create-ant-design-pro/template-react-ts/vite.config.ts
@@ -24,17 +24,5 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    proxy: {
-      // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-      '/api/': {
-        // 要代理的地址
-        target: 'https://proapi.azurewebsites.net/',
-        // 配置了这个可以从 http 代理到 https
-        // 依赖 origin 的功能可能需要这个，比如 cookie
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react(), antdLayout()],
 })
